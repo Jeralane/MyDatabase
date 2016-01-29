178269674bc47b2a30d965797aed70aecc3f8a41
Jess Alejo <jess.alejo@innohub.ph>
2016-01-29 22:21:37 +0800

    Initialize Project

---
+++ MyDatabase/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace MyDatabase
+{
+    public abstract class ActiveRecord<T> where T : ActiveRecord<T>, new()
+    {
+        public abstract string TableName { get; }
+
+        public int ID { get; set; }
+
+        protected abstract void SetProperties(DataTable dataTable);
+
+        public static T Find(int id)
+        {
+            return new T();
+        }
+
+        public static List<T> FindBy(string columnName, object value)
+        {
+            T model = new T();
+            var sqlBuilder = new StringBuilder();
+            sqlBuilder.Append("SELECT * FROM ");
+            sqlBuilder.AppendLine(model.TableName);
+            sqlBuilder.AppendFormat("WHERE {0} = ?{0}", columnName);
+            return new List<T>();
+        }
+
+        public virtual int Delete()
+        {
+            Console.WriteLine("DELETE FROM {0} WHERE ID = ?ID", TableName);
+            return 0;
+        }
+    }
+
+    public class User : ActiveRecord<User>
+    {
+        public override string TableName {get { return "users"; }}
+
+        protected override void SetProperties(DataTable dataTable)
+        {
+            ID = 1;
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+                // set properties
+            }
+        }
+    }
+
+
+}
---
+++ TestProject1/ActiveRecord_Find.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyDatabase;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class ActiveRecord_Find
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            User user = ActiveRecord<User>.Find(1);
+            Debug.Assert(user.ID == 1);
+        }
+    }
+}
